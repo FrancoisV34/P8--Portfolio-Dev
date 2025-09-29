ca3729a7a3296f15332783089a5100a821260f15
François <francois.vittecoq@osteopathe-eso.fr>
2025-09-29 22:15:54 +0200

    feat (jsx json and scss)
    create and update projects.json
    
    add props and import from json
    
    and display/layout

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,26 +1,53 @@
 import '../Style/Project.scss';
+import projects from '../Data/Projects.json';
 
 export default function Project() {
   return (
     <>
-      <article>
-        <div className="up">
-          <i className="kasa-app"></i>
-          <img
-            src="/moi.webp"
-            alt="Projet correspondant"
-            className="cap-ecran-projet"
-          />
-          <div className="technos">
-            Ceci sera l'emplacement des technos utilisées dans les projets
-            présentés
+      {projects.map((project) => (
+        <article>
+          <div className="up">
+            <i className="kasa-app"></i>
+            <img
+              src={project.image}
+              alt={project.imageAlt}
+              className="cap-ecran-projet"
+            />
+            <div className="technos">
+              {project.technos.map((techno, index) => (
+                <div className={techno.name} key={index}>
+                  <img
+                    key={index}
+                    src={techno.icon}
+                    alt={techno.name}
+                    className={techno.name}
+                  />
+                  <div
+                    className="competence-level"
+                    style={{
+                      border: `3px solid ${techno.color}`,
+                    }}
+                  >
+                    <div
+                      className="level"
+                      style={{
+                        width: `${techno.level}%`,
+                        backgroundColor: `${techno.color}`,
+                      }}
+                    ></div>
+                  </div>
+                  <span>{techno.name}</span>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="down">
-          Ici seront présents 1 menu déroulant avec la descritpion du projet + 1
-          menu lisatnt les compétences utilisées pour la réalisation du projet.
-        </div>
-      </article>
+          <div className="down">
+            Ici seront présents 1 menu déroulant avec la descritpion du projet +
+            1 menu lisatnt les compétences utilisées pour la réalisation du
+            projet.
+          </div>
+        </article>
+      ))}
     </>
   );
 }
